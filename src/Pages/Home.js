b3c667fdbef3b30525434f72e52a8eb83f2aa93f
Gabriel <Gabrielpedrofc@gmail.com>
2023-12-23 16:32:51 -0300

    Importação do MainFooter

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainNavbar from '../Components/MainNavbar';
+import MainFooter from '../Components/MainFooter';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <h2>Página Inicial</h2>
       {/* Conteúdo da página inicial */}
       <Link to="/Segunda">Core Rules</Link>
+      <MainFooter />
     </div>
   );
 };
