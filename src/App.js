deae88f51416176e93c425df66ec3005ee490a16
Gabriel <Gabrielpedrofc@gmail.com>
2023-12-23 16:43:52 -0300

    Formatações

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,15 +5,19 @@ import MainNavbar from '../Components/MainNavbar';
 import MainFooter from '../Components/MainFooter';
 
 const Home = () => {
-  return (
-    <div>
-        <MainNavbar />
-      <h2>Página Inicial</h2>
-      {/* Conteúdo da página inicial */}
-      <Link to="/Segunda">Core Rules</Link>
-      <MainFooter />
-    </div>
-  );
+    return (
+        <div>
+            <MainNavbar />
+
+            {/* Conteúdo da página inicial */}
+            <div class="spacer">
+                <h1>Teste SCSS</h1>
+                <p>Este é um teste básico para verificar se o SCSS está funcionando corretamente.</p>
+            </div>
+            <Link to="/Segunda">Core Rules</Link>
+            <MainFooter />
+        </div>
+    );
 };
 
 export default Home;
